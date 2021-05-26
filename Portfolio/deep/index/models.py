ce65c5cb27b5c2a931400c8d39da8965276e1249
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2021-05-26 22:02:02 +0545

    Get Project Data from admin

--- Portfolio/deep/index/models.py
+++ Portfolio/deep/index/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    project_id=models.IntegerField(primary_key=True)
+    project_description=models.CharField(max_length=100)
+    project_name=models.CharField(max_length=100)
+    project_status=models.IntegerField()
+
+    def __str__(self):
+        return self.project_name;
--- Portfolio/deep/index/views.py
+++ Portfolio/deep/index/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from index.models import Project
 # Create your views here.
 def home(request):
     context={
-        'title':"Home Page"
+        'title':"Home Page",
+        'project_name':Project.objects.all()
     };
+    
     return render(request,'index.html',context);
